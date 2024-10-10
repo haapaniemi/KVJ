bc17dad975a584b78f89452aa9c67f2c84b79adf
haapaniemi <156816740+haapaniemi@users.noreply.github.com>
2024-10-10 10:09:44 +0300

    Update water_temperatures.py

--- code/water_temperatures.py
+++ code/water_temperatures.py
@@ -218,9 +218,45 @@ if __name__ == '__main__':
     cbar = plt.colorbar(a)
 
     plt.show()
+
     
     
+    df = pd.read_csv('waterT_0.2m.csv')
+    print(df)
+    df['rolling_mean'] = df['values'].rolling(window=48).mean()
     
+    print(df)
+    
+   # df = df.dropna()
+    
+    instant = df['values']
+    rolling = df['rolling_mean']
+    
+    
+    print(rolling)
+    print(instant)
+    
+    
+    times = df.datetime.values
+    print(times)
+    
+    print(len(rolling))
+    print(len(instant))
+    print(len(times))
+    
+
+    #plt.scatter(pd.to_datetime(times), instant, marker='o',s=0.005,c='k',alpha=0.7)
+    #plt.scatter(pd.to_datetime(times), rolling, marker='x',s=0.005, c='r',alpha=0.7)
+    plt.subplots(2,1, sharex=True, sharey=True)
+    
+    plt.plot(pd.to_datetime(times), instant, alpha=0.7, c='k',label='30min values')
+    plt.plot(pd.to_datetime(times), rolling, alpha=0.7, c='r',label='daily')
+    plt.legend()
+    plt.show()
+    
+    
+
+
     
 #    mds = df.index.get_level_values(0).dt.strftime('%m%d')
 #    print(mds)
